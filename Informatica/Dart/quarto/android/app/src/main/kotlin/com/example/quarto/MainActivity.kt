0225704725e57b35b701f52450d0ec2210fa4ce6
RadResistant <119665556+RadResistant@users.noreply.github.com>
2023-11-29 00:55:07 +0100

    Delete Informatica/Dart directory

--- Informatica/Dart/quarto/android/app/src/main/kotlin/com/example/quarto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quarto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Informatica/Dart/ristorante/android/app/src/main/kotlin/com/example/ristorante/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ristorante
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
