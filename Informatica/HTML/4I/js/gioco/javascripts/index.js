04e5bade9e6e13e8ea8b689df11627b369b29775
RadResistant <raffaelesaponaro52@gmail.com>
2024-05-19 04:25:55 +0200

    Aggiornato gioco

--- Informatica/HTML/4I/js/gioco/javascripts/index.js
+++ Informatica/HTML/4I/js/gioco/javascripts/index.js
@@ -1,5 +1,13 @@
 "use strict";
 function centra(){
-    let immagine=document.querySelector("a");
-        immagine.style.top=window.innerHeight/2-immagine.height/2+"px";
+    let corpo=document.querySelector("body");
+    let immagine=document.querySelectorAll("img");
+    let div=document.querySelectorAll("div");
+    corpo.style.width=window.innerWidth;
+    corpo.style.height=window.innerHeight;
+    for(let i=0;i<immagine.length;i++){
+        immagine[i].style.position="fixed";
+        immagine[i].style.top=window.innerHeight/2-immagine[i].height/2+"px";
+        immagine[i].style.left=(div[i].width-immagine[i].width)/2+"px";
+    }
 }
