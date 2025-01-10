00a8be4fe07bf2498b804488a33522e59577daa6
RadResistant52 <raffaelesaponaro52@gmail.com>
2025-01-10 12:10:40 +0100

    Aggiornato il programma cities

--- TPSIT/5I/Server cities/script.js
+++ TPSIT/5I/Server cities/script.js
@@ -1,18 +1,24 @@
 "use strict";
 async function chiedi() {
     let nCitta=document.getElementById("nomeCitta").value;
-    let rispostaj = await fetch("http://localHost:8088/cities?city="+nCitta);
+    let rispostaj = await fetch("http://localhost:8088/?city="+nCitta);
     let risposta = await rispostaj.json();
     console.log(risposta);
     if(rispostaj.status==200 && rispostaj.ok==true){
         document.getElementById("listaCitta").removeChild;
         risposta.forEach(city => {
-            let p=document.createElement("p");
-            p.innerText=city.city;
-            document.getElementById("listaCitta").appendChild(p);
+            let opt=document.createElement("option");
+            opt.value=city.city;
+            document.getElementById("listaCitta").appendChild(opt);
         });
     }
     else{
         console.error("Errore "+ rispostaj.status)
     }
+}
+async function chiediR() {
+    let rispostaj = await fetch("http://localHost:8088/reservetions");
+    let risposta = await rispostaj.json();
+    console.log(rispostaj);
+    console.log(risposta);
 }
