aea53c25bd46e6e77c1a880f91b3b44403fde680
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-01-16 10:54:09 +0100

    Aggiornato il programma cities

--- TPSIT/5I/Server cities/script.js
+++ TPSIT/5I/Server cities/script.js
@@ -1,5 +1,14 @@
 "use strict";
 let cittaSelezionata;
+function aggiungiN(){
+    let menu=document.getElementById("numeroO");
+    for(let i=0; i<15;i++){
+        let opt=document.createElement("option");
+        opt.value=i+1;
+        opt.innerText=opt.value;
+        menu.appendChild(opt);
+    }
+}
 async function chiedi(e) {
     let tasto=e.key;
     if(tasto=="Enter"){
@@ -21,15 +30,19 @@ async function chiedi(e) {
 }
 async function prenota(){
     let cercato=document.getElementById("nomeCitta").value;
+    let nome=document.getElementById("nomeU").value;
+    let dataInizio=document.getElementById("dataI").value;
+    let dataFine=document.getElementById("dataF").value;
+    let ospiti=document.getElementById("numeroO").value;
     let rispostaj = await fetch("http://10.1.0.52:8088/cities?city="+cercato);
     let risposta = await rispostaj.json();
     console.log(risposta);
     let prenotazione={
         cityId: risposta[0].id,
-        from: "2023-01-01",
-        guests: 1,
-        name: "Edoardo",
-        to: "2023-01-10"
+        from: dataInizio,
+        guests: ospiti,
+        name: nome,
+        to: dataFine
     }
     fetch("http://10.1.0.52:8088/reservations",{
         method:'POST',
