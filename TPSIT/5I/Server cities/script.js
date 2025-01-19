0b129840b6ea8b73a066dd8c6aa3ed1d0ed210c9
RadResistant <raffaelesaponaro52@gmail.com>
2025-01-19 01:19:37 +0100

    Aggiornato il programma cities

--- TPSIT/5I/Server cities/script.js
+++ TPSIT/5I/Server cities/script.js
@@ -12,6 +12,7 @@ function aggiungiN(){
 async function chiedi(e) {
     let tasto=e.key;
     if(tasto=="Enter"){
+        document.getElementById("listaCitta").innerText="";
         let cercato=document.getElementById("nomeCitta").value;
         let rispostaj = await fetch("http://192.168.1.56:8088/cities?city="+cercato);
         let risposta = await rispostaj.json();
@@ -21,12 +22,12 @@ async function chiedi(e) {
                 let opt=document.createElement("option");
                 opt.value=city.city+"-"+city.country;
                 opt.dataset.cityId=city.id;
-                opt.addEventListener("click",()=>{
-                    cittaSelezionata=city;
-                })
+                opt.dataset.cityNname=city.city;
+                opt.dataset.cityCountry=city.country;
                 document.getElementById("listaCitta").appendChild(opt);
                 console.log(opt.dataset.cityId);
             });
+            seleziona();
         }
         else{
             console.error("Errore "+ rispostaj.status)
@@ -43,15 +44,12 @@ function mostraVoli(){
     document.getElementById("scelta").classList.add("hidden");
 }
 async function prenota(){
-    let p=document.createElement("p");
-    p.innerText="Riempire tutti i campi"
-    p.style.color="red";
-    p.id="errore";
     let cercato=document.getElementById("nomeCitta").value.trim();
     let nome=document.getElementById("nomeU").value.trim();
     let dataInizio=document.getElementById("dataI").value;
     let dataFine=document.getElementById("dataF").value;
     let ospiti=document.getElementById("numeroO").value;
+    document.getElementById("errore").classList.add("hidden");
     if(document.querySelectorAll("datalist option")[0]!=undefined && cercato!="" && nome!="" && dataInizio!="" && dataFine!="" && ospiti!=""){
         let idCitta=document.querySelectorAll("datalist option")[0];
         let prenotazione={
@@ -70,11 +68,25 @@ async function prenota(){
         });
     }
     else{
-        document.getElementById("prenotazioneHotel").appendChild(p);
+        document.getElementById("errore").classList.remove("hidden");
     }
 }
 async function controlla(){
     let rispostaj = await fetch("http://192.168.1.56:8088/reservations");
     let risposta = await rispostaj.json();
     console.log(risposta);
+}
+function seleziona(){
+    console.log("Entrato");
+    let opt=document.querySelectorAll("datalist option");
+    console.log("Preso opt "+opt.length +opt);
+    let nome=document.getElementById("nomeCitta").value;
+    console.log("Preso nome");
+    opt.forEach((opz)=>{
+        console.log("cerco");
+        if(opz.dataset.cityName==nome){
+            cittaSelezionata=opz;
+            console.log(opz);
+        }
+    }); 
 }
