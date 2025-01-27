fe519f0dc4ff2c29b48475e27194cbdfa7c94b99
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-01-27 11:46:28 +0100

    Aggiornato il programma sudoers

--- TPSIT/5I/Server cities/script.js
+++ TPSIT/5I/Server cities/script.js
@@ -1,21 +1,33 @@
 "use strict";
 let nome;
-function mostra(e){
+function mostra(e){      
     document.getElementById("erroreNome").classList.add("hidden");
     if(document.getElementById("nomeU").value!=""){
         nome=document.getElementById("nomeU").value;
         if(e.target.id=="pulsanteHotel"){
             aggiungiN();
             document.getElementById("prenotazioneHotel").classList.remove("hidden");
+            document.getElementById("scelta").classList.add("hidden");
+            document.getElementById("pulsanteHome").classList.remove("hidden");
         }
         if(e.target.id=="pulsanteVoli"){
             document.getElementById("prenotazioneVoli").classList.remove("hidden");
+            document.getElementById("scelta").classList.add("hidden");
+            document.getElementById("pulsanteHome").classList.remove("hidden");
         }
         if(e.target.id=="pulsanteRicerca"){
             ricercaPrenotazioni();
             document.getElementById("ricerca").classList.remove("hidden");
+            document.getElementById("scelta").classList.add("hidden");
+            document.getElementById("pulsanteHome").classList.remove("hidden");
         }
-        document.getElementById("scelta").classList.add("hidden");
+        if(e.target.id=="pulsanteHome"){
+            document.getElementById("ricerca").classList.add("hidden");
+            document.getElementById("prenotazioneVoli").classList.add("hidden");
+            document.getElementById("prenotazioneHotel").classList.add("hidden");
+            document.getElementById("pulsanteHome").classList.add("hidden");
+            document.getElementById("scelta").classList.remove("hidden");
+        }
     }
     else{
         document.getElementById("erroreNome").classList.remove("hidden");
@@ -32,15 +44,29 @@ function aggiungiN(){
 }
 async function cercaCitta(e) {
     if(e.key=="Enter"){
-        document.getElementById("listaCitta").innerText="";
-        let cercato=document.getElementById("nomeCitta").value;
-        let rispostaj = await fetch("http://localhost:8088/cities?city="+cercato);
+        let cercato;
+        let lista;
+        if(e.target.id=="nomeCitta"){
+            lista=document.getElementById("listaCitta");
+            cercato=document.getElementById("nomeCitta").value;
+        }
+        if(e.target.id=="nomeCittaPartenza"){
+            lista=document.getElementById("listaCittaPartenza");
+            cercato=document.getElementById("nomeCittaPartenza").value;
+        }
+        if(e.target.id=="nomeCittaArrivo"){
+            lista=document.getElementById("listaCittaArrivo");
+            cercato=document.getElementById("nomeCittaArrivo").value;
+        }
+        lista.innerHTML="";
+        let rispostaj = await fetch("http://10.1.0.52:8088/cities?city="+cercato);
         let risposta = await rispostaj.json();
         if(rispostaj.status==200 && rispostaj.ok==true){
             risposta.forEach((city)=>{
                 let opt=document.createElement("option");
-                opt.value=city.city+"-"+city.country;
-                document.getElementById("listaCitta").appendChild(opt);
+                opt.innerText=city.city+"-"+city.country;
+                opt.dataset.idCitta=city.id;
+                lista.appendChild(opt);
             });
         }
         else{
@@ -49,96 +75,58 @@ async function cercaCitta(e) {
     }
 }
 async function prenotaHotel(){
-    let cercato=document.getElementById("nomeCitta").value.trim().split("-");
+    let idCitta=document.getElementById("listaCitta").dataset.idCitta;
     let dataInizio=document.getElementById("dataI").value;
     let dataFine=document.getElementById("dataF").value;
     let ospiti=document.getElementById("numeroO").value;
     document.getElementById("erroreHotel").classList.add("hidden");
-    if(cercato[0]!="" && nome!="" && dataInizio!="" && dataFine!="" && ospiti!=""){
-        let cittaCercata = await fetch("http://localhost:8088/cities?city="+cercato[0]+"&country="+cercato[1]);
-        if(cittaCercata.status==200 && cittaCercata.ok==true){
-            cittaCercata = await cittaCercata.json();
-            if(cittaCercata.length>=0){
-                let prenotazione={
-                    cityId: cittaCercata[0].id,
-                    from: dataInizio,
-                    guests: ospiti,
-                    name: nome,
-                    to: dataFine
-                };
-                fetch("http://localhost:8088/reservations",{
-                    method:'POST',
-                    headers:{
-                        'Content-Type': 'application/json'
-                    },
-                    body:JSON.stringify(prenotazione)
-                });
-            }
-        }
+    if(idCitta!="" && nome!="" && dataInizio!="" && dataFine!="" && ospiti!=""){
+        let prenotazione={
+            cityId: idCitta,
+            from: dataInizio,
+            guests: ospiti,
+            name: nome,
+            to: dataFine
+        };
+        fetch("http://10.1.0.52:8088/reservations",{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(prenotazione)
+        });
     }
     else{
         document.getElementById("erroreHotel").classList.remove("hidden");
     }
 }
+function assegnaH(e){
+    let idCitta=e.target.dataset.idCitta;
+    document.getElementById("listaCitta").dataset.idCitta=idCitta;
+}
 async function controllaH(){
-    let rispostaj = await fetch("http://localhost:8088/reservations");
+    let rispostaj = await fetch("http://10.1.0.52:8088/reservations");
     let risposta = await rispostaj.json();
     console.log(risposta);
-}
-async function cercaCittaVolo(e) {
-    if(e.key=="Enter"){
-        if(e.target.id=="nomeCittaPartenza"){
-            document.getElementById("listaCittaPartenza").innerText="";
-            let cercato=document.getElementById("nomeCittaPartenza").value;
-            let rispostaj = await fetch("http://localhost:8088/cities?city="+cercato);
-            let risposta = await rispostaj.json();
-            if(rispostaj.status==200 && rispostaj.ok==true){
-                risposta.forEach((city)=>{
-                    let opt=document.createElement("option");
-                    opt.value=city.city+"-"+city.country;
-                    document.getElementById("listaCittaPartenza").appendChild(opt);
-                });
-            }
-            else{
-                console.error("Errore "+ rispostaj.status)
-            }
-        }
-        if(e.target.id=="nomeCittaArrivo"){
-            document.getElementById("listaCittaArrivo").innerText="";
-            let cercato=document.getElementById("nomeCittaArrivo").value;
-            let rispostaj = await fetch("http://localhost:8088/cities?city="+cercato);
-            let risposta = await rispostaj.json();
-            if(rispostaj.status==200 && rispostaj.ok==true){
-                risposta.forEach((city)=>{
-                    let opt=document.createElement("option");
-                    opt.value=city.city+"-"+city.country;
-                    document.getElementById("listaCittaArrivo").appendChild(opt);
-                });
-            }
-            else{
-                console.error("Errore "+ rispostaj.status)
-            }
-        }
-    }
 }
 async function cercaVolo() {
     document.getElementById("listaVoli").innerText="";
     let partenza=document.getElementById("nomeCittaPartenza").value.split("-");
     let arrivo=document.getElementById("nomeCittaArrivo").value.split("-");
-    let rispostaPartenza = await fetch("http://localhost:8088/cities?city="+partenza[0]+"&country="+partenza[1]);
+    let rispostaPartenza = await fetch("http://10.1.0.52:8088/cities?city="+partenza[0]+"&country="+partenza[1]);
     let rispostaArrivo;
     if(arrivo!=""){
-        rispostaArrivo = await fetch("http://localhost:8088/cities?city="+arrivo[0]+"&country="+arrivo[1]);
+        rispostaArrivo = await fetch("http://10.1.0.52:8088/cities?city="+arrivo[0]+"&country="+arrivo[1]);
     }
     if(rispostaPartenza.status==200 && rispostaPartenza.ok==true){
         rispostaPartenza = await rispostaPartenza.json();
         let rispostav;
         if(arrivo!=""){ 
             rispostaArrivo = await rispostaArrivo.json();
-            rispostav = await fetch("http://localhost:8088/flights?from="+rispostaPartenza[0].id+"&to="+rispostaArrivo[0].id);
+            rispostav = await fetch("http://10.1.0.52:8088/flights?from="+rispostaPartenza[0].id+"&to="+rispostaArrivo[0].id);
         }
         else{
-            rispostav = await fetch("http://localhost:8088/flights?from="+rispostaPartenza[0].id);
+            rispostav = await fetch("http://10.1.0.52:8088/flights?from="+rispostaPartenza[0].id);
         }
         if(rispostav.ok==true && rispostav.status==200){
             rispostav = await rispostav.json();
@@ -169,7 +157,7 @@ function prenotaVolo(e){
             date:dataPartenza
         };
         console.log(ticket);
-        fetch("http://localhost:8088/tickets",{
+        fetch("http://10.1.0.52:8088/tickets",{
             method:'POST',
             headers:{
                 'Content-Type': 'application/json'
@@ -186,40 +174,42 @@ function assegnaV(e){
     document.getElementById("listaVoli").dataset.idVolo=idVolo;
 }
 async function controllaV(){
-    let rispostaj = await fetch("http://localhost:8088/tickets");
+    let rispostaj = await fetch("http://10.1.0.52:8088/tickets");
     let risposta = await rispostaj.json();
     console.log(risposta);
 }
 async function ricercaPrenotazioni() {
-    let hotel = await fetch("http://localhost:8088/reservations?name="+nome);
+    document.getElementById("ricerca").innerHTML="";
+    let hotel = await fetch("http://10.1.0.52:8088/reservations?name="+nome);
     if(hotel.ok==true){
         hotel = await hotel.json();
         hotel.forEach(async (reservation)=>{
-            let citta= await fetch("http://localhost:8088/cities/"+reservation.cityId);
+            let citta= await fetch("http://10.1.0.52:8088/cities/"+reservation.cityId);
             citta=await citta.json();
             document.getElementById("ricerca").innerHTML+=
-            `<span>
+            `<section>
                 <h1>Hotel ${citta.city}</h1>
+                <p>Ospiti:${reservation.guests}</p>
                 <p>Check in:${reservation.from}</p>
                 <p>Check out:${reservation.to}</p>
                 <p>Paese:${citta.country}</p>
-            </span>`;
+            </section>`;
         });
     }
-    let voli = await fetch("http://localhost:8088/tickets?name="+nome);
+    let voli=await fetch("http://10.1.0.52:8088/tickets?name="+nome);
     if(voli.ok==true){
-        voli= await voli.json();
+        voli=await voli.json();
         voli.forEach(async (ticket)=>{
-            let volo= await fetch("http://localhost:8088/flights/"+ticket.flightId);
+            let volo= await fetch("http://10.1.0.52:8088/flights/"+ticket.flightId);
             volo=await volo.json();
             document.getElementById("ricerca").innerHTML+=
-            `<span>
-            <h1>Volo ${ticket.flightId}</h1>
+            `<section>
+                <h1>Volo ${ticket.flightId}</h1>
                 <p>Data di partenza:${ticket.date}</p>
                 <p>Data di arrivo:${ticket.date}</p>
                 <p>Paese di partenza:${volo.from.city}</p>
                 <p>Paese di arrivo:${volo.to.city}</p>
-            </span>`;
+            </section>`;
         });
     }
 }
