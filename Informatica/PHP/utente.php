3e27a430cc0500b0c2f52b42817be34e4a843f2c
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-03-17 15:57:46 +0100

    Create utente.php

---
+++ Informatica/PHP/utente.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get">
+        <input type="text" name="email">
+        <input type="password" name="password">
+        <input type="submit" name="tasto">
+    </form>
+    <?php
+        if(isset($_GET["tasto"])){
+            if(strtolower($_GET["email"])=="paolo" && strtolower($_GET["password"])=="rossi"){
+                echo "<h1> Ciao ".$_GET["email"]."</h1>";
+            }
+            else{
+                echo "<h1>Accesso negato</h1>";
+            }
+        }
+    ?>
+</body>
+</html>
