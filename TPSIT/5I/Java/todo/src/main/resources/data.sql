369dc57148d356e5f0d4dce99d684e2c8a22fe2c
RadResistant52 <raffaelesaponaro52@gmail.com>
2025-04-04 12:46:52 +0200

    creato il progetto todo

---
+++ TPSIT/5I/Java/todo/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO todo (lista, cosa, fatto) VALUES ('casa', 'pulire', false);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('casa', 'comperare fagioli', false);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('hobby', 'pagare palestra', true);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('hobby', 'pulire scarpe', true);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('lavoro', 'studiare Napoleone', false);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('lavoro', 'consegnare relazione', true);
+INSERT INTO todo (lista, cosa, fatto) VALUES ('lavoro', 'pulire armadietto', false);
---
+++ TPSIT/5I/Java/todo/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todo(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lista VARCHAR(20),
+    cosa VARCHAR(200),
+    fatto BOOLEAN
+);
