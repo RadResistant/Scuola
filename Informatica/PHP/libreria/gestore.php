8669c6eb0735a5adb6e33f5b1235c262beebbeab
RadResistant <raffaelesaponaro52@gmail.com>
2025-04-11 00:34:48 +0200

    Update gestore.php

--- Informatica/PHP/libreria/gestore.php
+++ Informatica/PHP/libreria/gestore.php
@@ -23,24 +23,24 @@
     </form>
     <?php
         if(isset($_GET["aggiungiAutore"])){
-            ?>
+    ?>
             <h2>Inserimento autori</h2>
             <form action="gestore.php">
                 <input type="text" name="nomeAutore" placeholder="Inserisci il nome">
                 <input type="text" name="cognomeAutore" placeholder="Inserisci il cognome">
                 <input type="text" name="cfAutore" placeholder="Inserisci il cf">
-                <button name="aAutore">Registra</button>
+                <button name="registraAutore">Registra</button>
             </form>
-            <?php
+    <?php
         }
-        if(isset($_GET["aAutore"])){
+        if(isset($_GET["registraAutore"])){
             if(!empty($_GET["nomeAutore"]) && !empty($_GET["cognomeAutore"]) && !empty($_GET["cfAutore"])){
                 $nome=htmlspecialchars($_GET["nomeAutore"]);
                 $cognome=htmlspecialchars($_GET["cognomeAutore"]);
                 $cf=strtoupper(htmlspecialchars($_GET["cfAutore"]));
                 $inserimentoAutore="INSERT INTO autori(cf,nome,cognome) VALUES ('".$cf."','".$nome."','".$cognome."');";
                 if(!mysqli_query($conn,$inserimentoAutore)){
-                    echo "Error: " . $queryInserimento . "<br>" . $conn->error;
+                    echo "Error: " . $inserimentoAutore . "<br>" . $conn->error;
                 }
                 else{
                     echo "<p>Autore registrato con successo</p>";
@@ -48,6 +48,98 @@
             }
             mysqli_close($conn);
         }
+        if(isset($_GET["aggiungiCategoria"])){
+    ?>
+            <h2>Inserimento Categorie</h2>
+            <form action="gestore.php">
+                <input type="text" name="descrizioneCategoria" placeholder="Inserisci la categoria">
+                <button name="registraCategoria">Registra</button>
+            </form>
+    <?php
+        }
+        if(isset($_GET["registraCategoria"])){
+            if(!empty($_GET["descrizioneCategoria"])){
+                $descrizione=htmlspecialchars($_GET["descrizioneCategoria"]);
+                $inserimentoCategoria="INSERT INTO categorie(descrizione) VALUES ('".$descrizione."');";
+                if(!mysqli_query($conn,$inserimentoCategoria)){
+                    echo "Error: " . $inserimentoCategoria . "<br>" . $conn->error;
+                }
+                else{
+                    echo "<p>Categoria registrata con successo</p>";
+                }
+            }
+            mysqli_close($conn);
+        }
+    ?>
+    <?php
+        if(isset($_GET["registraAutore"])){
+                if(!empty($_GET["nomeAutore"]) && !empty($_GET["cognomeAutore"]) && !empty($_GET["cfAutore"])){
+                    $nome=htmlspecialchars($_GET["nomeAutore"]);
+                    $cognome=htmlspecialchars($_GET["cognomeAutore"]);
+                    $cf=strtoupper(htmlspecialchars($_GET["cfAutore"]));
+                    $inserimentoAutore="INSERT INTO autori(cf,nome,cognome) VALUES ('".$cf."','".$nome."','".$cognome."');";
+                    if(!mysqli_query($conn,$inserimentoAutore)){
+                        echo "Error: " . $inserimentoAutore . "<br>" . $conn->error;
+                    }
+                    else{
+                        echo "<p>Autore registrato con successo</p>";
+                    }
+                }
+                mysqli_close($conn);
+            }
+            if(isset($_GET["aggiungiLibro"])){
+    ?>
+                <h2>Inserimento autori</h2>
+                <form action="gestore.php">
+                    <input type="text" name="nomeLibro" placeholder="Inserisci il nome del libro">
+                    <select name="autoreLibro">
+                        <?php
+                            $ricercaAutori="SELECT * FROM autori";
+                            $result=mysqli_query($conn,$ricercaAutori);
+                            if(mysqli_num_rows($result)>0){
+                                while($autore=mysqli_fetch_assoc($result)){
+                                    echo "<option value=".$autore["cf"].">".$autore["nome"]." ".$autore["cognome"]."</option>";
+                                }
+                            }
+                        ?>
+                    </select>
+                    <select name="categoriaLibro">
+                        <?php
+                            $ricercaCategorie="SELECT * FROM categorie";
+                            $result=mysqli_query($conn,$ricercaCategorie);
+                            if(mysqli_num_rows($result)>0){
+                                while($categoria=mysqli_fetch_assoc($result)){
+                                    echo "<option value=".$categoria["id"].">".$categoria["descrizione"]."</option>";
+                                }
+                            }
+                        ?>
+                    </select>
+                    <button name="registraLibro">Registra</button>
+                </form>
+    <?php
+            }
+            if(isset($_GET["registraLibro"])){
+                if(!empty($_GET["nomeLibro"]) && !empty($_GET["autoreLibro"]) && !empty($_GET["categoriaLibro"])){
+                    $nome=htmlspecialchars($_GET["nomeLibro"]);
+                    $autore=htmlspecialchars($_GET["autoreLibro"]);
+                    $categoria=htmlspecialchars($_GET["categoriaLibro"]);
+                    $inserimentoLibro="INSERT INTO libri(nome) VALUES ('".$nome."');";
+                    $test=mysqli_query($conn,$inserimentoLibro);
+                    if(!$test){
+                        echo "Error: " . $inserimentoLibro . "<br>" . $conn->error;
+                    }
+                    else{
+                        $ricercaAutori="SELECT * FROM libri WHERE libri.nome='".$nome."' LIMIT 1;";
+                        $result=mysqli_query($conn,$ricercaAutori);
+                        if(mysqli_num_rows($result)>0){
+                            while($autore=mysqli_fetch_assoc($result)){
+                                print_r($autore);
+                            }
+                        }
+                    }
+                }
+                mysqli_close($conn);
+            }
     ?>
 </body>
 </html>
