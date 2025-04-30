20dac273349cacd1454f536285b1462b6733240e
RadResistant <raffaelesaponaro52@gmail.com>
2025-04-30 02:36:35 +0200

    Aggiornato il programma eventi

---
+++ Informatica/PHP/eventi/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-in eventi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        include "info.php";
+        $conn=mysqli_connect($server,$uname,$password,$db);
+        if(!$conn){
+            die("Errore connessione al db nce arpova");
+        }
+    ?>
+    <div>
+        <form>
+            <input type="text" name="email">
+            <input type="password" name="passw">
+            <button name="login">Accedi</button>
+            <?php
+                if(isset($_GET["login"])){
+                    if(!empty($_GET["email"]) && !empty($_GET["passw"])){
+                        $ricercaUtente="SELECT * FROM utenti WHERE email = '".$_GET["email"]."';";
+                        $resultUtente=mysqli_query($conn,$ricercaUtente);
+                        if(mysqli_num_rows($resultUtente)=1){
+                            $utente=mysqli_fetch_assoc($resultUtente);
+                            if(password_verify($_GET["passw"],$utente["password"])){
+                                echo "bravo";
+                            }
+                        }
+                    }
+                    else{
+                        echo "<p class='error'>Riempi tutti i campi</p>";
+                    }
+                }
+            ?>
+            <p>Non sei registrato?</p>
+            <button name="signup">Registrati</button>
+        </form>    
+    </div>
+</body>
+</html>
+<?php
+    if(isset($_GET["signup"])){
+        header("Location:signup.php");
+    }
+?>
---
+++ Informatica/PHP/eventi/info.php
@@ -0,0 +1,5 @@
+<?php
+    $server="localhost:3306";
+    $uname="saponaroi";
+    $password="saponaro";
+    $db="saponaro_eventi";
