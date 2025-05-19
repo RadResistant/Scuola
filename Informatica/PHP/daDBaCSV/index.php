9b84b701109165def1c9cda70604d62d7292d646
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-05-19 15:36:36 +0200

    Creato il programma che converte i dati da un db a un file csv

---
+++ Informatica/PHP/daDBaCSV/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convertitore</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Sto facendo</h1>";
+        $path=$_SERVER["DOCUMENT_ROOT"]."/daDBaCSV/files/";
+        // So che bastava mettere il /files/ prima del nome del file da aprire ma cosi almeno mi ricordo qualcosa di nuovo
+        include "info.php";
+        $conn=mysqli_connect($servername,$username,$passw,$db);
+        if(!$conn){
+            die(mysqli_connect_err());
+        }
+        $ricercaTabelle="SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = '".$db."';";
+        $resultTabelle=mysqli_query($conn,$ricercaTabelle);
+        if(mysqli_num_rows($resultTabelle)>0){
+            while($tabella=mysqli_fetch_assoc($resultTabelle)){
+                $file=fopen($path.$tabella["TABLE_NAME"].".csv","w");
+                $ricercaContenuto="SELECT * FROM ".$tabella["TABLE_NAME"].";";
+                $resultContenuto=mysqli_query($conn,$ricercaContenuto);
+                if(mysqli_num_rows($resultContenuto)>0){
+                    while($riga=mysqli_fetch_assoc($resultContenuto)){
+                        $testo=implode(",",$riga)."\n";
+                        fwrite($file,$testo);
+                    }
+                }
+            }
+        }
+        echo "<h1>Fatto</h1>";
+    ?>
+</body>
+</html>
---
+++ Informatica/PHP/daDBaCSV/info.php
@@ -0,0 +1,5 @@
+<?php
+    $servername="10.1.0.52:8306";
+    $username="saponaro";
+    $passw="saponaro";
+    $db="saponaro_Negozio";
