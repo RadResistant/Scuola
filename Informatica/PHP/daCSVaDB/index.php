2c5becfe70bfce5105961965935c59183eeaee9f
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-05-21 13:21:07 +0200

    Aggiornato il programma da CSV a DB

--- Informatica/PHP/daCSVaDB/index.php
+++ Informatica/PHP/daCSVaDB/index.php
@@ -15,13 +15,36 @@
         $file=fopen("moto.csv","r");
         $fileInfo=pathinfo("moto.csv");
         $fileContent=explode("\n",fread($file,filesize("moto.csv")));
-        print_r($fileContent);
-        $dbName="saponaro_".$fileInfo["filename"];
-        $creazioneDB="CREATE DATABASE ".$dbName;
-        if(!mysqli_query($conn,$creazioneDB)){
-            echo "no";
+        $tabelle=array();
+        foreach($fileContent as $key=>$content){
+            if($key!=0){
+                $tabelle[]=explode(",",$content);
+            }
         }
-        $creazioneTabella="";
+        $dbName="saponaro_".$fileInfo["filename"];
+        $ricercaDB="SHOW DATABASES WHERE database='".$dbName."';";
+        $resultDB=mysqli_query($conn,$ricercaDB);
+        if(mysqli_num_rows($resultDB)==0){
+            $creazioneDB="CREATE DATABASE ".$dbName;
+            if(!mysqli_query($conn,$creazioneDB)){
+                echo "no";
+            }
+        }
+        $ricercaTabelle="SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = '".$dbName."' AND TABLE_NAME=".$fileContent[0].";";
+        $resultTabelle=mysqli_query($conn,$ricercaTabelle);
+        if(mysqli_num_rows($resultTabelle)==0){
+            $creazioneTabelle="CREATE TABLE ".$dbName.".".$fileContent[0]."(
+                ".$tabelle[0][0]." ".$tabelle[0][2]."(".$tabelle[0][1]."),
+                ".$tabelle[0][3]." ".$tabelle[0][5]."(".$tabelle[0][4]."),
+                ".$tabelle[0][6]." ".$tabelle[0][8]."(".$tabelle[0][7]."),
+            )";
+            if(!mysqli_query($conn,$creazioneTabelle)){
+                echo "no";
+            }
+        }
+        $inserimentoDati="INSERT INTO ".$dbName.".".$fileContent[0]."(
+            
+        );";
     ?>
 </body>
 </html>
--- Informatica/PHP/daCSVaDB/info.php
+++ Informatica/PHP/daCSVaDB/info.php
@@ -1,5 +1,5 @@
 <?php
-    $servername="localhost:3306";
-    $username="saponaroi";
+    $servername="10.1.0.52:8306";
+    $username="saponaro";
     $passw="saponaro";
     $db="saponaro_moto";
