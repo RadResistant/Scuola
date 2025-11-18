186a3d495ae5c8897760b1c4b37d266b705fc94c
RadResistant <119665556+RadResistant@users.noreply.github.com>
2025-11-18 17:57:02 +0100

    Update soluzione.c

--- Universita/ProgrammazioneProcedurale/cifratore/soluzione.c
+++ Universita/ProgrammazioneProcedurale/cifratore/soluzione.c
@@ -1,9 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+void cleanString(char *dpt){
+    char plaintext[25]="";
+    for(int i=0,ipt=0;dpt[i]!='\0' || ipt<(strlen(dpt)-1);i++){
+        if((dpt[i]>='a' && dpt[i]<='z') || (dpt[i]>='A' && dpt[i]<='Z') ){
+            plaintext[ipt++] = dpt[i];
+        }
+    }
+    plaintext[sizeof(plaintext)-1] = '\0';
+    strcpy(dpt, plaintext);
+}
 int main(){
-    // printf(">Cifratore di Vernam<");
-    // char frase[20]="";
-    // scanf("Inserire il testo in chiaro",frase);
-    char a='N';
-    char b='P';
-    printf("%c\n",a^b);
+    int size=50;
+    char *plaintext = malloc(size);
+    char *key= malloc(size);
+    printf(">Cifratore di Vernam<\n");
+    printf("Scrivi la tua frase: ");
+    fgets(plaintext, size, stdin);
+    cleanString(plaintext);
+    printf("output: %s\n", plaintext);
+    printf("Scrivi la tua chiave di cifratura: ");
+    fgets(key, size, stdin);
+    cleanString(key);
+    size=24;
+    plaintext = realloc(plaintext, size);
+    printf("output: %s\n", plaintext);
+    printf("key: %s\n", key);
 }
